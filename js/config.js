f150b62ce91708b3f7ff645fda0b55688290c725
Adarsha Karki <111886610+Adarshakarki@users.noreply.github.com>
2026-02-21 08:42:21 +0545

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-export const INSTANCES = [
-  'https://inv.nadeko.net',
-  'https://invidious.fdn.fr',
-  'https://yt.artemislena.eu',
-  'https://invidious.nerdvpn.de',
-  'https://iv.melmac.space'
-];
